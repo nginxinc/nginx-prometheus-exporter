57da659a8e7b107a2e6d91f7c3f05346988c006e
mirecl <grazhdankov@gmail.com>
2021-07-07 17:42:15 +0300

    Perfomance parseStubStats

--- client/nginx.go
+++ client/nginx.go
@@ -1,12 +1,11 @@
 package client
 
 import (
+	"bytes"
 	"context"
 	"fmt"
 	"io"
 	"net/http"
-	"strconv"
-	"strings"
 )
 
 // NginxClient allows you to fetch NGINX metrics from the stub_status page.
@@ -76,69 +75,43 @@ func (client *NginxClient) GetStubStats() (*StubStats, error) {
 }
 
 func parseStubStats(data []byte, stats *StubStats) error {
-	dataStr := string(data)
-
-	parts := strings.Split(dataStr, "\n")
-	if len(parts) != 5 {
-		return fmt.Errorf("invalid input %q", dataStr)
+	if bytes.Count(data, []byte("\n")) != 4 {
+		return fmt.Errorf("invalid input %s", data)
 	}
 
-	activeConsParts := strings.Split(strings.TrimSpace(parts[0]), " ")
-	if len(activeConsParts) != 3 {
-		return fmt.Errorf("invalid input for active connections %q", parts[0])
+	cnt, value := 0, int64(0)
+	for _, s := range data {
+		if s >= '0' && s <= '9' {
+			value *= 10
+			value += int64(s & 0x0f)
+			continue
+		}
+
+		if value > 0 {
+			switch cnt {
+			case 0:
+				stats.Connections.Active = value
+			case 1:
+				stats.Connections.Accepted = value
+			case 2:
+				stats.Connections.Handled = value
+			case 3:
+				stats.Requests = value
+			case 4:
+				stats.Connections.Reading = value
+			case 5:
+				stats.Connections.Writing = value
+			case 6:
+				stats.Connections.Waiting = value
+			}
+			value = 0
+			cnt++
+		}
 	}
 
-	actCons, err := strconv.ParseInt(activeConsParts[2], 10, 64)
-	if err != nil {
-		return fmt.Errorf("invalid input for active connections %q: %w", activeConsParts[2], err)
+	if cnt != 7 {
+		return fmt.Errorf("invalid input %s", data)
 	}
-	stats.Connections.Active = actCons
-
-	miscParts := strings.Split(strings.TrimSpace(parts[2]), " ")
-	if len(miscParts) != 3 {
-		return fmt.Errorf("invalid input for connections and requests %q", parts[2])
-	}
-
-	acceptedCons, err := strconv.ParseInt(miscParts[0], 10, 64)
-	if err != nil {
-		return fmt.Errorf("invalid input for accepted connections %q: %w", miscParts[0], err)
-	}
-	stats.Connections.Accepted = acceptedCons
-
-	handledCons, err := strconv.ParseInt(miscParts[1], 10, 64)
-	if err != nil {
-		return fmt.Errorf("invalid input for handled connections %q: %w", miscParts[1], err)
-	}
-	stats.Connections.Handled = handledCons
-
-	requests, err := strconv.ParseInt(miscParts[2], 10, 64)
-	if err != nil {
-		return fmt.Errorf("invalid input for requests %q: %w", miscParts[2], err)
-	}
-	stats.Requests = requests
-
-	consParts := strings.Split(strings.TrimSpace(parts[3]), " ")
-	if len(consParts) != 6 {
-		return fmt.Errorf("invalid input for connections %q", parts[3])
-	}
-
-	readingCons, err := strconv.ParseInt(consParts[1], 10, 64)
-	if err != nil {
-		return fmt.Errorf("invalid input for reading connections %q: %w", consParts[1], err)
-	}
-	stats.Connections.Reading = readingCons
-
-	writingCons, err := strconv.ParseInt(consParts[3], 10, 64)
-	if err != nil {
-		return fmt.Errorf("invalid input for writing connections %q: %w", consParts[3], err)
-	}
-	stats.Connections.Writing = writingCons
-
-	waitingCons, err := strconv.ParseInt(consParts[5], 10, 64)
-	if err != nil {
-		return fmt.Errorf("invalid input for waiting connections %q: %w", consParts[5], err)
-	}
-	stats.Connections.Waiting = waitingCons
 
 	return nil
 }
